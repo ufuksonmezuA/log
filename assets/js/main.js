a571ae861ac7943346108de5de07f210e828def0
UFUK AYHAN SÖNMEZ <67274624+ufuksonmezuA@users.noreply.github.com>
2022-06-19 11:52:21 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,3 +97,45 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
+/*=============== SWIPER POPULAR ===============*/
+var swiperPopular = new Swiper('.popular__container', {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: false,
+    slidesPerView: 'auto',
+    loop: false,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+/*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value__accordion-item');
+
+accordionItems.forEach((item) =>{
+  const accordionHeader = item.querySelector('.value__accordion-header');
+  accordionHeader.addEventListener('click', () =>{
+    const openItem = document.querySelector('.accordion-open');
+    toggleItem(item);
+    if(openItem && openItem!== item){
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) =>{
+  const accordionContent = item.querySelector('.value__accordion-content');
+  
+  if(item.classList.contains('accordion-open')){
+    accordionContent.removeAttribute('style');
+    item.classList.remove('accordion-open');
+  }
+  else{
+    accordionContent.style.height = accordionContent.scrollHeight + 'px';
+    item.classList.add('accordion-open');
+  }
+};
